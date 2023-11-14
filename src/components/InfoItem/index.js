8415c00522a06947c969f1f32b75bedd0ee2453d
Bulgantamir Lkhagvajav <79170631+bulgantamirl@users.noreply.github.com>
2023-11-14 19:50:21 +0800

    heh

--- src/components/InfoItem/index.js
+++ src/components/InfoItem/index.js
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import logo from "../../../public/assets/logoBlack.svg";
 import Link from "next/link";
+import {useEffect} from "react";
+import AOS from "aos";
+import 'aos/dist/aos.css';
 
 export default function InfoItem({src, title, desc, link, ...props}) {
+    useEffect(()=> {
+        AOS.init();
+        const handleRouteChange = () => {
+            AOS.refresh();
+        };
+
+        window.addEventListener('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            window.removeEventListener('routeChangeComplete', handleRouteChange);
+        };
+    },[])
     return (
-        <Link className={'w-[400px] bg-white md:w-full lg:min-w-[800px] lg:w-[800px] p-6 rounded-xl hover:shadow-lg'} href={link} >
+        <Link className={'w-[400px] bg-white md:w-full lg:min-w-[800px] lg:w-[800px] p-6 rounded-xl hover:shadow-lg'} href={link}  data-aos="fade-up" data-aos-duration="800" >
             <div className={`flex lg:flex-row w-full lg:max-w-[800px] gap-6 lg:h-[150px] overflow-hidden md:flex-col md:w-full md:h-auto`} {...props}>
                 <div className={'w-[220px] h-[125px]'}>
                     <Image className={'w-full object-cover'} src={src} alt={''} />
